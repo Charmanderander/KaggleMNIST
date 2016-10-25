4c65d75b27e45b8140b28cb958e185f5a7f3a142
chan <jinhao_@hotmail.com>
2016-10-25 21:21:51 +0800

    changed code for pca

--- scripty_PCA.py
+++ scripty_PCA.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cross_validation import train_test_split
@@ -10,17 +11,16 @@ print "reading in data"
 # Reading in the dataset
 df = pd.read_csv('train.csv', nrows=28000)
 Y = df['label'].values
-X = df.drop('label', axis=1).values    
+X = df.drop('label', axis=1).values
+x_test = pd.read_csv('test.csv')
 
 min_max_scaler = MinMaxScaler() # Create the MinMax object.
 X = min_max_scaler.fit_transform(X.astype(float)) # Scale pixel intensities only.
-
-x_train, x_test, y_train, y_test = train_test_split(X, Y, 
-                        test_size = 0.2, random_state = 0) # Split training/test.
+x_test = min_max_scaler.fit_transform(x_test.astype(float)) # Scale pixel intensities only.
 
 # Using Neural Net
 print "creating model"
-model = DBN([x_train.shape[1], 700, 10],
+model = DBN([X.shape[1], 700, 10],
                 learn_rates = 0.1,
                 learn_rate_decays = 0.9,
                 epochs = 100,
@@ -29,19 +29,21 @@ model = DBN([x_train.shape[1], 700, 10],
 
 print "doing PCA"
 pca = PCA()
-X_transformed = pca.fit_transform(x_train)
-model.fit(X_transformed, y_train)
+X_transformed = pca.fit_transform(X)
+
+print "fitting data"
+model.fit(X_transformed, Y)
 
 x_test_transformed = pca.transform(x_test)
 
-#predicted = model.predict(x_test_transformed)
-
-print "fitting data"
-#model.fit(x_train, y_train)
-
 print "predicting test"
-#predicted = model.predict(x_test)
+predicted = model.predict(x_test_transformed)
 
-y_true, y_pred = y_test, model.predict(x_test_transformed) # Get our predictions
-print(classification_report(y_true, y_pred)) # Classification on each digit
-print 'The accuracy is:', accuracy_score(y_true, y_pred)
+print "writing results"
+submission = DataFrame(predicted, columns=['Label'], 
+                       index=np.arange(1, 28001))
+submission.index.names = ['ImageId']
+
+submission.to_csv('submission.csv')
+
+print "Done!"
